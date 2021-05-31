7a74c4c5c925ec94c5914e10fceecf73aadf9c76
pankaj.k.jha <pankaj.k.jha@accenture.com>
2021-05-31 21:36:35 +0800

    #Added Android image

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:calculator/util/Constants/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'Screens/landing_page.dart';
 
 void main() {
@@ -11,6 +12,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: appTheme.primaryColor, // status bar color
+    ));
     return MaterialApp(
       title: 'Calculator',
       theme: appTheme,
